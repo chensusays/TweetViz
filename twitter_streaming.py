0238f41a8fb38eb195002739ce63eaddb5d60736
chensusays <chensusays@gmail.com>
2016-12-13 14:41:22 -0500

    formatting

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -9,15 +9,6 @@ except ImportError:
 #from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
 
-
-#### https://github.com/chrisalbon/election_day_2016_twitter ###
-## Used to retrieve all tweets related to Election Day 2016 ##
-## Approximately  6,546,824 total tweets about the elction occured on Nov. 8th ##
-
-## Since the job would take too long to run myself I found an existing dataset @ ##
-### https://www.kaggle.com/kinguistics/election-day-tweets ###
-
-
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
